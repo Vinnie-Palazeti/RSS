91e01346baef64628f6ba40cf1bd393882809cc0
Vinnie Palazeti <vinnie.palazeti@gmail.com>
2021-02-20 13:09:24 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="RSS",  # Replace with your own username
+    version="0.0.0",
+    author="Vinnie Palazeti",
+    author_email="vinnie.palazeti@gmail.com",
+    description="cmd line RSS",
+    packages=setuptools.find_packages(),
+    #install_requires=[
+    #    'pymongo',
+    #    'dnspython',
+    #    'pandas',
+    #    'numpy',
+    #    'pybbda'
+    #],
+    python_requires='>=3.6',
+)
